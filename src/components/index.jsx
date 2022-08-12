3b74cd2b823d587b6ea99288ba33a000b82418be
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-13 00:49:03 +0200

    Setup project structure

---
+++ src/components/index.jsx
@@ -0,0 +1,4 @@
+export * from "~/components/footer"
+export * from "~/components/header"
+export * from "~/components/projects-list"
+export * from "~/components/theme-toggle"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
