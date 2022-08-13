072e6b46c8eb94a401df7368cd741566f03c847a
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-13 02:23:52 +0200

    Create hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Header, Footer } from "~/components"
+import { Header, Hero, Footer } from "~/components"
 
 export const App = () => {
   return (
     <div className="max-w-3xl mx-auto px-6 h-screen flex flex-col justify-between">
       <Header />
+      <div>
+        <Hero />
+      </div>
       <Footer />
     </div>
   )
---
+++ src/components/hero.jsx
@@ -0,0 +1,8 @@
+export const Hero = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-5xl font-bold">freeCodeCamp Projects</h1>
+      <p className="text-xl">Here is a list of project I completed so far:</p>
+    </div>
+  )
+}
