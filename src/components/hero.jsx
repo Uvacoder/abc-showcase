f83c155cbe5b9e8cc3d3b60e3ac7e32490dad034
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-13 03:04:04 +0200

    Update hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,8 @@
 export const Hero = () => {
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="text-5xl font-bold">freeCodeCamp Projects</h1>
-      <p className="text-xl opacity-70">
+      <h1 className="text-4xl font-bold">freeCodeCamp Projects</h1>
+      <p className="text-lg opacity-70">
         Here is a list of project I completed so far:
       </p>
     </div>
