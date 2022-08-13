a241a8c45f6919ec9eca4d81af6b113276bcfd19
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-13 03:04:48 +0200

    Create projects list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { Header, Hero, Footer } from "~/components"
+import { Header, Hero, ProjectsList, Footer } from "~/components"
 
 export const App = () => {
   return (
     <div className="max-w-3xl mx-auto px-6 h-screen flex flex-col justify-between">
       <Header />
-      <div>
+      <div className="flex flex-col gap-12 mt-24 mb-12">
         <Hero />
+        <ProjectsList />
       </div>
       <Footer />
     </div>
--- src/components/projects-list.jsx
+++ src/components/projects-list.jsx
@@ -1,3 +1,32 @@
+import { project } from "~/data"
+
 export const ProjectsList = () => {
-  return <div></div>
+  return (
+    <div className="flex flex-col divide-y-[1px] divide-black/10 dark:divide-white/10">
+      {project.map(project => (
+        <div
+          key={project.title}
+          className="py-8 flex justify-between items-center"
+        >
+          <div className="flex flex-col gap-3">
+            <h2 className="font-bold text-lg">{project.title}</h2>
+            <h3 className="text-rose-500 dark:text-sky-500 text-sm">
+              {project.path}
+            </h3>
+          </div>
+          <div className="flex gap-6">
+            {project.links.map(link => (
+              <a
+                key={link.label}
+                href={link.url}
+                className="hover:text-rose-500 dark:hover:text-sky-500 hover:underline underline-offset-8"
+              >
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
